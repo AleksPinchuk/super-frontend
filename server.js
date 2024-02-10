bbec261cb2191d03fddf426a69d36d59aeb1ce29
Sasha <aleksvladpinchuk@gmail.com>
2024-02-10 16:45:59 +0300

    wtf

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
 
 const http = require('http');
 const fs = require('fs');
-
-const server = http.createServer(function(reguest, response){
-    console.log(reguest.method, reguest.url);
-    if (reguest.url === '/style.css'){
+const { hostname } = require('os');
+const server = http.createServer(function(request, response){
+    console.log(request.method, request.url);
+    if (request.url === '/style.css'){
         const css = fs.readFileSync('style.css', 'utf8');
-        response.end(css);  
+        response.end(css);
     } else {
-    const html = fs.readFileSync('index.html', 'utf8');
-    response.end(html);
+        const html = fs.readFileSync('index.html', 'utf8');
+        response.end(html);
     }
 });
-
+const port = process.env.PORT || 5000;
 console.log(process.env.PORT);
-
-server.listen(process.env.PORT);
-console.log('Server started!');
+server.listen(port, { hostname: hostname }, () => {
+    console.log('Server started!');
+});
